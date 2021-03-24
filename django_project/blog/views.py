6562f65b20c29cefece443f99e68615d84bbd83b
Marcelo Giacometti da Silva <48400319+giacomettisss@users.noreply.github.com>
2021-03-23 22:09:15 -0300

    update 2021032301

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
+posts = [
+    {
+        'author' : 'CoreyMs',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27,2028'
+    },
+    {
+        'author' : 'Marcelo',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28,2028'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
