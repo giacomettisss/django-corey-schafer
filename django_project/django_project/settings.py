a49d4238e1de25d35cbdf0f849d2a5250bdd77fc
Marcelo Giacometti da Silva <48400319+giacomettisss@users.noreply.github.com>
2021-03-29 01:11:25 -0300

    update 2021032901
    adding password reset

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -130,3 +130,15 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER =  os.environ.get('DJANGO_EMAIL_USER')
+EMAIL_HOST_PASSWORD =  os.environ.get('DJANGO_EMAIL_PASS')
+
+# ONLY TO TEST
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
