ced19ac381971e88fa7bc6a02ce6616e06e776b9
Marcelo Giacometti da Silva <48400319+giacomettisss@users.noreply.github.com>
2021-03-29 23:20:32 -0300

    2021030915

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j1bf()xy#d=a8!gq4qb-raz61tts6ic)vpxmy%j3b9#t(@08*1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-corey-schafer2.herokuapp.com'
+]
 
 
 # Application definition
